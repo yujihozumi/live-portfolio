fe18825c28fee570250a0653fe9a7cbd1e2acfc6
yujihozumi <yuu.60011@gmail.com>
2022-03-04 16:08:48 +0900

    プロフィール編集ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   root 'pages#home'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
