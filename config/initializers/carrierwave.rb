7d56e45fb6d5e2e108e59698f61effa6f7bd2a11
yujihozumi <yuu.60011@gmail.com>
2022-03-10 15:19:14 +0900

    aws,host設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = ENV['S3_BUCKET']
+  config.asset_host = 'https://s3-us-west-2.amazonaws.com/ENV['S3_BUCKET']'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
