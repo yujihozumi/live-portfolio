91d4fffffc1f25eb531796077ca885be9bf0917e
yujihozumi <yuu.60011@gmail.com>
2022-03-11 15:24:50 +0900

    error修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
   config.fog_directory  = ENV['S3_BUCKET']
   config.asset_host = ENV['S3_HOST_NAME']
   config.fog_credentials = {
-    :provider 'AWS',
-    :aws_access_key_id ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key ENV['AWS_SECRET_ACCESS_KEY'],
-    :region ENV['AWS_DEFAULT_REGION']
+    :provider => 'AWS',
+    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => ENV['AWS_DEFAULT_REGION']
   }
 end
