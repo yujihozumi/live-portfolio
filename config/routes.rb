f3829fade08ede7a1f8899121b7845ac394d0e55
yujihozumi <yuu.60011@gmail.com>
2022-03-13 15:37:53 +0900

    ゲストユーザー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    end
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
   resources :posts, only: %i(index new create show destroy) do
