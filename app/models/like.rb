1b743c47c605c23ddd2219bb5574461bc9d15c74
yujihozumi <yuu.60011@gmail.com>
2022-04-05 16:51:36 +0900

    バリデーション追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user_id, uniqueness: { scope: :post_id }
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :post_id },  presence: true
+  validates :post_id, uniqueness: { scope: :user_id }, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :photos, dependent: :destroy
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :photos
+
+  validates :title,          presence: true
+  validates :live_venue,     presence: true
+  validates :artist,         presence: true
+  validates :start_time,     presence: true
+  validates :ending_time,     presence: true
+  validates :start_date,     presence: true
+  validates :body,           presence: true
 
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
